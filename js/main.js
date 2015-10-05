f23f743ccbf0d83e0c213ba689de0f4354c95aa3
Ilya Buziuk <ibuziuk@exadel.com>
2015-10-05 20:35:20 +0300

    Adding blocks №2 & №3

---
+++ js/main.js
@@ -0,0 +1,75 @@
+var images = [];
+images[0] = "gallery/pic0.jpg";
+images[1] = "gallery/pic1.jpg";
+images[2] = "gallery/pic2.jpg";
+images[3] = "gallery/pic3.jpg";
+images[4] = "gallery/pic4.jpg";
+images[5] = "gallery/pic5.jpg";
+
+// image counter / switcher
+var x = 0;
+var imageTimer;
+
+function getTimeOfYear() {
+    var timeOfYear;
+    var d = new Date();
+    var m = d.getMonth();
+
+    if (m === 11 || (m >= 0 && m < 2)) {
+        timeOfYear = "Winter";
+    } else if (m >= 2 && m < 5) {
+        timeOfYear = "Spring";
+    } else if (m >= 5 && m < 8) {
+        timeOfYear = "Summer";
+    } else if (m >= 8 && m < 11) {
+        timeOfYear = "Autumn";
+    }
+
+    return timeOfYear;
+}
+
+function setImageHeader() {
+    var src;
+    var img = document.getElementById("year_time");
+    var timeOfYear = getTimeOfYear();
+    if (timeOfYear === "Winter") {
+        img.src = "images/winter.jpg";
+    } else if (timeOfYear === "Spring") {
+        img.src = "images/spring.jpg";
+    } else if (timeOfYear === "Summer") {
+        img.src = "images/summer.jpg";
+    } else if (timeOfYear === "Autumn") {
+        img.src = "images/outumn.jpg";
+    }
+
+    if (src) {
+        img.src = src;
+    }
+}
+
+function changeImage() {
+    var img = document.getElementById("year_time");
+    img.src = images[x];
+    x++;
+
+    if(x >= images.length) {
+        x = 0;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    // Block №2
+    setImageHeader();
+
+    // Block #3
+    document.getElementById("view_presentation").addEventListener("click", function() {
+        if (imageTimer) {
+            clearInterval(imageTimer);
+            this.textContent = "Start Presentation";
+            imageTimer = null 
+        } else {
+            imageTimer = setInterval("changeImage()", 3000);
+            this.textContent = "Stop Presentation";
+        }
+    });
+});
