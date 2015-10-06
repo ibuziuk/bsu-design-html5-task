d6e84fc1af4eeb9be9d7efbb7c5165e933392749
Ilya Buziuk <ibuziuk@exadel.com>
2015-10-06 15:23:44 +0300

    Adding Block №6

--- js/main.js
+++ js/main.js
@@ -57,6 +57,28 @@ function changeImage() {
     }
 }
 
+function getTimeToNewYear() {
+    var timeToNewYear = new Object();
+    var current = new Date();
+    var newYear = new Date("01.01.2016");
+    var left = newYear - current;
+
+    var formatter = left / 1000;
+    seconds = left % 60;
+    formatter /= 60;
+    var minutes = formatter % 60;
+    formatter /= 60;
+    var hours = formatter % 24;
+    formatter /= 24;
+    var days = formatter;
+
+    timeToNewYear.days = Math.round(days);
+    timeToNewYear.hours = Math.round(hours);
+    timeToNewYear.minutes = Math.round(minutes);
+    timeToNewYear.seconds = Math.round(seconds); 
+    return timeToNewYear;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     // Block №2
     setImageHeader();
@@ -78,6 +100,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById("read_more_text").textContent = "А знаем ли мы, какие растения вокруг нас? Конечно, все узнают дуб, березу, ель, клен... А трава? Присмотритесь к ней повнимательнее, и вы увидите большое разнообразие видов. Одни зацветают рано весной и цветут до осени. Другие летом отцветают, но их побеги еще долго зеленеют. Есть растения, что зацветают лишь в разгар лета."
     }); 
 
+    // Block №4
+    document.getElementById("read_more").addEventListener("click", function() {
+        document.getElementById("read_more_text").textContent = "А знаем ли мы, какие растения вокруг нас? Конечно, все узнают дуб, березу, ель, клен... А трава? Присмотритесь к ней повнимательнее, и вы увидите большое разнообразие видов. Одни зацветают рано весной и цветут до осени. Другие летом отцветают, но их побеги еще долго зеленеют. Есть растения, что зацветают лишь в разгар лета."
+    }); 
+
     // Block №8
     document.getElementById("news_1").addEventListener("click", function() {
         this.textContent = "В городе, особенно большом, с его многочисленными заводами и фабриками, воздух, вода и почва загрязнены промышленными отходами, выхлопными газами автомобилей, бытовым мусором. Растения же очищают среду, в которой мы живем. Особенно важны те растения, которые не только накапливают в себе вредные вещества, но и способны так их изменить (переработать), что те становятся безопасными для человека и животных."
@@ -85,6 +112,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.getElementById("news_2").addEventListener("click", function() {
         this.textContent = "Многие знакомые нам растения представляют интерес как лекарственные и пищевые. Правда, в городе их собирать нельзя. Но умение узнавать полезное растение среди множества других пригодится вам, когда вы окажетесь в деревне или на даче, в отдалении от автомобильных магистралей. Только в экологически чистых районах можно собирать растения для употребления в пищу и для лекарственных целей."
-    });  
+    });
+    
+    // Block №6
+    document.getElementById("clock").addEventListener("mouseover", function() {
+        var timeLeft = getTimeToNewYear();
+        var timeText = "До Нового Года осталось: " + timeLeft.days + " дней " + timeLeft.hours + " часов " + timeLeft.minutes + " минуты"
+        this.title = timeText;
+        document.getElementById("time_left").textContent = timeText;
+    });
+
+    document.getElementById("clock").addEventListener("mouseout", function() {
+        this.title = "";
+        document.getElementById("time_left").textContent = "";
+    }); 
 
 });
